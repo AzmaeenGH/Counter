8534f9b07423fff16b82ef8c683d5d29fa370f79
AzmaeenGH <119947997+AzmaeenGH@users.noreply.github.com>
2023-07-05 23:57:00 +0600

    Add files via upload

---
+++ Counter.sol
@@ -0,0 +1,19 @@
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+contract Counter{
+    uint public count;
+
+   function increment() external{
+       count +=1;
+    }
+    
+   function decrement() external{
+       count -=1;
+    }
+
+}
+
+
